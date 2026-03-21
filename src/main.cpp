a4c3da4a8837d457f3ea8ec15e29af852ff27893
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-03-21 13:29:18 -0700

    removed the logical parser checking with actual implementation of parser to func files without logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 
 #include "Lexer.h"
 #include "Tokens.h"
+#include "Parser.h"
 
 int main(int argc, char* argv[]) {
 
     //makes sure theres at least one input file
-    if (argc < 2) {
+    if (argc < 3) {
         std::cout << "Usage: " << argv[0] << " <input_file> [output_file]" << std::endl;
         return 1;
     }
 
     std::string inputFileName = argv[1];
+    std::string outputFileName = argv[2];
 
     // create the lexer using the input file name
     Lexer lexer(inputFileName);
@@ -23,40 +25,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // if there's a second arguemnt it will print out the oupt to that file instead of terminal
-    std::ofstream outFile;
-    bool useOutFile = false;
-
-    if (argc >= 3) {
-        std::string outputFileName = argv[2];
-        outFile.open(outputFileName);
-        if (!outFile.is_open()) {
-            std::cerr << "Error: could not open output file: " << outputFileName << std::endl;
-            return 1;
-        }
-        useOutFile = true;
+    std::ofstream outFile(outputFileName);
+    if (!outFile.is_open()) {
+        std::cerr << "Error: could not open output file: " << outputFileName << std::endl;
+        return 1;
     }
 
-    std::ostream& out = useOutFile ? static_cast<std::ostream&>(outFile) : std::cout;// this is for either a file or temrnal as output 
+    Parser parser(lexer, outFile, true);
+    parser.Rat26S();
 
-
-    out << "Token\t\tLexeme" << std::endl;
-    out << "-----------------------------" << std::endl;
-
-    // calls lexer until we hit end of file 
-    while (true) {
-        Token t = lexer.lexer(); //gets next token 
-
-        if (t.tokenCategory == T_FileEnd) {   //stops when we reahc end of file 
-            break;
-        }
-
-        out << tokenName(t.tokenCategory) << "\t\t" << t.lexeme << std::endl;
-    }
-
-    if (useOutFile) {
-        outFile.close(); //closes output file 
-    }
+    outFile << "Parsing finished without syntax errors." << std::endl;
+    outFile.close();
 
     return 0;
 }
